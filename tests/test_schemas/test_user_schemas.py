583dae6f4ccd40dd6b7b1168243d5db9a3cbb87d
sso927 <sso9@njit.edu>
2024-11-29 04:18:58 -0500

    fixed another KeyError issue from pytest

--- tests/test_schemas/test_user_schemas.py
+++ tests/test_schemas/test_user_schemas.py
@@ -9,7 +9,8 @@ def test_user_base_valid(user_base_data):
     user = UserBase(**user_base_data)
     assert user.email == user_base_data["email"]
 
-    assert user.nickname == user_base_data["nickname"]
+    if 'nickname' in user_base_data:
+        assert user.nickname == user_base_data["nickname"]
 
 # Tests for UserCreate
 def test_user_create_valid(user_create_data):
